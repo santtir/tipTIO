99e2e4cb8a3d8540ae34acd8874a68910c12c4a6
Lautaro Gallo <lautarovg147@gmail.com>
2021-10-14 17:53:25 -0300

    agregado de js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use stric"
+let btn = document.querySelector("#btn-enviar");
+btn.addEventListener("click", verificarformulario);
+
+let respuesta = document.querySelector("#respuesta");
+
+
+
+
+function verificarformulario(){
+    
+
+
+}
